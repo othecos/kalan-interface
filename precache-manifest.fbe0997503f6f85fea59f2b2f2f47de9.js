4a340ffad167a091cc3caf2d88b508de9f89a9ba
Otavio Henrique Pires Costa <othecos@ibm.com>
2020-11-11 16:41:16 -0300

    Updates

---
+++ precache-manifest.fbe0997503f6f85fea59f2b2f2f47de9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded6c809cc9caf2ebaa114a82a543615",
+    "url": "/kalan-interface/index.html"
+  },
+  {
+    "revision": "42e59f6ee6157603cbb3",
+    "url": "/kalan-interface/static/css/2.6fecc2cc.chunk.css"
+  },
+  {
+    "revision": "cd63538927a9739c747c",
+    "url": "/kalan-interface/static/css/main.2b289f01.chunk.css"
+  },
+  {
+    "revision": "42e59f6ee6157603cbb3",
+    "url": "/kalan-interface/static/js/2.0034e944.chunk.js"
+  },
+  {
+    "revision": "fbf516d7d05fa0e1c2a2db87093b3941",
+    "url": "/kalan-interface/static/js/2.0034e944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f734aa21dc6800796f7c",
+    "url": "/kalan-interface/static/js/3.5c6502f5.chunk.js"
+  },
+  {
+    "revision": "cd63538927a9739c747c",
+    "url": "/kalan-interface/static/js/main.ffb11b15.chunk.js"
+  },
+  {
+    "revision": "e8a7ccc597674b6c5316",
+    "url": "/kalan-interface/static/js/runtime-main.7bb675f5.js"
+  }
+]);
